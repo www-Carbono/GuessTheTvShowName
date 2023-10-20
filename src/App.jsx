2eff3604f33d9215a760a39186e51be9405d59fb
“Antonio” <dev.antonio.gonzalez.parla@gmail.com>
2023-10-20 18:51:09 +0200

    remove comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 import { Game } from './components/game'
 
 function App () {
-  // DIseño erminado solo queda hacerlo responsive para pcs (esta listo ahora mismo para mobile-first)
   return (
     <>
       <Game />
